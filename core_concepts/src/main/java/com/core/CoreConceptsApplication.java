8c210ca6a863abb907d5f2f07b7579521e0e7aef
mrrat1601@gmail.com <ndhule@niteen>
2021-08-29 22:44:17 +0530

    Structural commit for core concept spring boot project CMT_1

---
+++ core_concepts/src/main/java/com/core/CoreConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreConceptsApplication.class, args);
+	}
+
+}
